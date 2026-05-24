8874dcb5513c52a9e8c6a351bf56754cabcb419d
delaporte <dlprtln@gmail.com>
2026-05-24 18:38:52 +0200

    ajout images

--- src/consts.ts
+++ src/consts.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Robot tondeuse diy  Dobby1';
-export const SITE_DESCRIPTION = 'Réalisation d'un robot tondeuse pour tondre un verger'
+export const SITE_DESCRIPTION = "Réalisation d'un robot tondeuse pour tondre un verger";
 export const SITE_KEYWORDS = 'robot, tondeuse, diy, do it yourself, dobby1, verger';
 
