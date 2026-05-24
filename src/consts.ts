840a04b2e14a54b17d575e672c549b01bc64b8c7
delaporte <dlprtln@gmail.com>
2026-05-24 11:59:39 +0200

    titre

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Robot tondeuse diy  Dobby1';
+export const SITE_DESCRIPTION = 'Réalisation d'un robot tondeuse pour tondre un verger'
+export const SITE_KEYWORDS = 'robot, tondeuse, diy, do it yourself, dobby1, verger';
+
